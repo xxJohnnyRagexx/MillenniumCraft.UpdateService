bd5a3118d05eb59c415fc4bf45a90dedad8bbd83
Sergey Tihomirov <ragej16@gmail.com>
2023-02-13 22:50:06 +0400

    Добавьте файлы проекта.

---
+++ Data/Entities/UpdateItemEntity.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data.Entities
+{
+    public class UpdateItemEntity
+    {
+        /// <summary>
+        /// Gets or sets PK represented as integer.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets version of package.
+        /// </summary>
+        public string InnerVersion { get; set; }
+
+        /// <summary>
+        /// Gets or sets version of game, which would be modified.
+        /// </summary>
+        public string GameVersion { get; set; }
+
+        /// <summary>
+        /// Gets or sets description of update.
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the final URL, that can be used for downloading of package by client.
+        /// </summary>
+        public string Url { get; set; }
+    }
+}
